d5a7976e6c47bef2cebbf8b5e4984b36d91d5bec
unknown <reskypratamahamzah498@gmail.com>
2025-10-30 15:40:31 +0800

    first commit

---
+++ webgl/lib/script.js
@@ -0,0 +1,56 @@
+let gl, shaderProgram;
+
+
+function initGL() {
+    let canvas = document.getElementById("canvas");
+    console.log(canvas);
+    gl = canvas.getContext("webgl"); 
+    gl.viewport(0, 0, canvas.width, canvas.height); 
+    gl.clearColor(1, 1, 1, 1); 
+}
+
+function createShaders() {
+    let vs = "";
+    vs += "void main() {"; 
+    vs += " gl_Position = vec4(0.5, 0.5, 0.0, 1.0);"; 
+    vs += " gl_PointSize = 100.0;";                   
+    vs += "}";
+
+    let vertexShader = gl.createShader(gl.VERTEX_SHADER)
+    gl.shaderSource(vertexShader, vs);
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        console.error("Vertex Shader Error:", gl.getShaderInfoLog(vertexShader));
+    }
+
+    let fs = "";
+    fs += "void main() {";
+    fs += " gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);";
+    fs += "}";
+
+    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fs);
+    gl.compileShader(fragmentShader);
+    
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        console.error("Fragment Shader Error:", gl.getShaderInfoLog(fragmentShader));
+    }
+
+
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+}
+
+function draw() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+window.onload = function() {
+    initGL();
+    createShaders();
+    draw();
+};
